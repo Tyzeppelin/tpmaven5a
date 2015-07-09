766d31c36e1e91b11c8beb5eeadbc5a0bd989049
francois boschet <fboschet@tetram.irisa.fr>
2015-07-09 11:32:05 +0200

    tuturu

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,16 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+
+	/**
+	 * Main method
+	 * @param args
+	 */
+    public static void main(final String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
